9e4832e077c746dde72accfb7c137441c7e87ff1
Turu0512 <hinataptyan@gmail.com>
2021-06-15 16:01:47 +0900

    カレンダー末尾の来月の日付

--- calendar/main.js
+++ calendar/main.js
@@ -24,12 +24,13 @@ for(let i=0; i<n; i++){
 console.log(dates);
 }
 
+
 function getCalendarBody(){
   const dates =[]; //date:日付, day: 曜日
   const lastDate = new Date(year, month + 1, 0).getDate();
   //末日は、来月の１日の一日前の日付ということで０をしていする。
   //慣習で月は０から始まり、日付は１から始まる為
-
+  
   for (let i = 1;i<=lastDate; i++){
     dates.push({
       date:i,
@@ -40,6 +41,24 @@ function getCalendarBody(){
   console.log(dates);
 }
 
-getCalendarHead();
+function getCalendarTail(){//来月のカレンダーのデータ
+  const dates = [];
+  const lastDay = new Date(year,month+1, 0).getDay();
+
+  for (let i =1;i<7-lastDay;i++){
+    dates.push({
+      date:i,
+      isToday:false,
+      isDisabled: true,
+
+    });
+
+  }
+console.log(dates);
+}
+
+
+getCalendarTail();
+// getCalendarHead();
 // getCalendarBody();
 }
