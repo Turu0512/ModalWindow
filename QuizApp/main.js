3f8f3c584ed200a53dede87f9804b4eda7041ed8
Turu0512 <hinataptyan@gmail.com>
2021-06-18 10:50:10 +0900

    完成

--- QuizApp/main.js
+++ QuizApp/main.js
@@ -3,16 +3,18 @@
 const question = document.getElementById(`question`)
 const choices = document.getElementById(`choices`)
 const btn = document.getElementById(`btn`)
+const result = document.getElementById(`result`)
+const scoreLabel = document.querySelector(`#result > p`)
 
-const quizSet = [
+const quizSet = shuffle([//出題順をシャッフル
   {q:`What is A?`, c:[`A0`,`A1`,`A2`]},
   {q:`What is b?`, c:[`b0`,`b1`,`b2`]},
   {q:`What is c?`, c:[`c0`,`c1`,`c2`]},
   
-];
+]);
 let currentNum = 0;
 let isAnswered;
-
+let score = 0;
 
 function shuffle(arr) {//渡された配列の入れ替えをしている
   for(let i= arr.length -1;i > 0; i--) {//最後の要素i
@@ -30,13 +32,14 @@ function checkAnswer(li) {
   }
 isAnswered=true;
 
-  if(li.textContent === quizSet[currentNum].c[0]){
+  if(li.textContent === quizSet[currentNum].c[0]){//0番目が正解になる
     li.classList.add(`correct`)
+    score++;
   }else{
     li.classList.add(`wrong`)
   }
 
-  
+  btn.classList.remove(`disabled`);
 }
 
 function setQuiz(){
@@ -44,6 +47,12 @@ function setQuiz(){
   //checkAnswer(li)が実行されることで、すべてtrueに書き換わる。
 
   question.textContent = quizSet[currentNum].q;
+
+while(choices.firstChild){//よく使われるテクニック
+  choices.removeChild(choices.firstChild);
+}//（）内の条件がNullかfalseになるまでループする
+//中身がなくなるまで繰り返すということ
+
   const shuffledChoices =shuffle([...quizSet[currentNum].c]);
   //配列のコピーを渡す。shuffle(quizSet[currentNum].c)では参照を渡してしまうため、中身の配列まで変わってしまう。
   // console.log(quizSet[currentNum].c)
@@ -56,9 +65,27 @@ checkAnswer(li);
     });
     choices.appendChild(li);
   });
+  if(currentNum === quizSet.length-1){//最後の問題でNextの表示を変える
+    btn.textContent = 'Show Score';
+  }
 }
 
 setQuiz();
 
+btn.addEventListener(`click`,()=>{
+  if(btn.classList.contains(`disabled`)){
+    return;
+  }
+  btn.classList.add(`disabled`);
 
+  if(currentNum === quizSet.length - 1){
+console.log(`Score:${score}/${quizSet.length}`);
+scoreLabel.textContent = `Score:${score}/${quizSet.length}`;
+result.classList.remove(`hidden`);
+  }else{
+
+    currentNum++;
+    setQuiz();
+  }
+})
 }
