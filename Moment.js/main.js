0499a594cf3c7dd2776e054030f6bd90e709f446
Turu0512 <hinataptyan@gmail.com>
2021-06-21 21:36:03 +0900

    表示される形式の指定　公式サイトの使い方

--- Moment.js/main.js
+++ Moment.js/main.js
@@ -1,8 +1,7 @@
 (()=>{
 'use strict'
-// const d = moment();
-// const d = moment('01-10-2018','MM-DD-YYYY')
-const d =moment([2018,0,10])
-console.log(d.format('LLLL'));
-
+const d = moment('2018-01-10');
+d.locale('en')//日本語のSrcをコメントアウトしてもいいが、一時的に表示を変えたいとき。
+console.log(d.format('MMMM Do, dddd'));
+//公式サイト、右上のDocs→Display→Formatで表示のさせ方
 })();
