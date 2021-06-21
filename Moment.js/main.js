f49afd2e9b4df57dbacf6b68cc94513ee382ea37
Turu0512 <hinataptyan@gmail.com>
2021-06-21 21:30:27 +0900

    日付の入れ方

--- Moment.js/main.js
+++ Moment.js/main.js
@@ -1,14 +1,8 @@
 (()=>{
 'use strict'
+// const d = moment();
+// const d = moment('01-10-2018','MM-DD-YYYY')
+const d =moment([2018,0,10])
+console.log(d.format('LLLL'));
 
-const input = document.querySelector('input')
-const button = document.querySelector('button')
-const p = document.querySelector('p')
-
-button.addEventListener('click',()=>{
-// const d = new Date(input.value);
-//   p.textContent = `${d.getFullYear()}年${d.getMonth()+1}月${d.getDate()}日`;
-const d = moment(input.value);
-p.textContent = d.isValid() ? d.format('LL') : '無効な日付です';
-});
 })();
