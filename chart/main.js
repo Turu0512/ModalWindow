26f098fe91f31aa26ef4ea5d8066b224136d4a9b
Turu0512 <hinataptyan@gmail.com>
2021-06-21 20:35:35 +0900

    chart_js

---
+++ chart/main.js
@@ -0,0 +1,41 @@
+
+(function(){
+    'use strict'
+
+    let type = 'line';
+    let data = {
+      labels:[2010,2011,2012,2013],
+      datasets:[{
+        label: '@taguchi',
+        data: [120,300,200,210],
+        borderColor:'red',
+        borderWidth:5,
+      } ,{
+        label: '@koji',
+      data: [180,250,320,180],
+      borderColor:'blue',
+        borderWidth:5,
+    }]
+  };
+
+  let options= {
+    scales:{
+yAxes: [{
+  ticks:{
+    suggestedMin:0,
+    suggestedMax:400
+  }
+}]
+    }
+  };
+
+    let ctx = document.getElementById('my_chart').getContext('2d');
+    let myChart = new Chart(ctx,{
+      type:type,
+      data:data,
+      options:options
+    });
+  })();
+
+
+
