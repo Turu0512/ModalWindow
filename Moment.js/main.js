2b40f37e5e007c799a5520321b7c4fd672ae3cb9
Turu0512 <hinataptyan@gmail.com>
2021-06-22 07:48:56 +0900

    次の誕生日の求め方

--- Moment.js/main.js
+++ Moment.js/main.js
@@ -15,8 +15,30 @@ const now = moment();
 const age = now.diff(bday,'year')
 const days = now.diff(bday,'day')
 
-return `今、${age}歳です！生まれてから${days}日経ちました！`
+/*
+now = 2021-06-22
+
+bday: 2000-06-22-> this: 2021-06-23 -> next: 2022-06-23
+bday: 2000-06-23-> this: 2021-06-24 -> next: 2022-06-24
+bday: 2000-06-24-> this: 2021-06-25 -> next: 2021-06-25
+*/
+
+const thisBirthday = bday.clone().year(now.year());
+//yearだけ今年に書き換える
+let nextBirthday;
+
+if(now.isSameOrAfter(thisBirthday)){//同じか後か
+  //日付で比べる
+  nextBirthday = thisBirthday.clone().add(1,'year');
+}else{
+  nextBirthday = thisBirthday;
 }
+
+return `今、${age}歳です！生まれてから${days}日経ちました！次の誕生日は${nextBirthday.format('LL')}`;
+}
+
+
+
 
 button.addEventListener('click',()=>{
   p.textContent = getResult();
