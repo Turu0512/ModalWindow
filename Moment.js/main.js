379a7d2b5f1d086bc9645278b4fa5c1515024ca0
Turu0512 <hinataptyan@gmail.com>
2021-06-21 21:15:00 +0900

    Moment.js導入

--- Moment.js/main.js
+++ Moment.js/main.js
@@ -6,7 +6,9 @@ const button = document.querySelector('button')
 const p = document.querySelector('p')
 
 button.addEventListener('click',()=>{
-const d = new Date(input.value);
-  p.textContent = `${d.getFullYear()}年${d.getMonth()+1}月${d.getDate()}日`;
+// const d = new Date(input.value);
+//   p.textContent = `${d.getFullYear()}年${d.getMonth()+1}月${d.getDate()}日`;
+const d = moment(input.value);
+p.textContent = d.isValid() ? d.format('LL') : '無効な日付です';
 });
 })();
