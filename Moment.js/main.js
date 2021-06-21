c76823d4f9c51c0d6dc160be369ea9cc063b260d
Turu0512 <hinataptyan@gmail.com>
2021-06-21 21:50:53 +0900

    色々な表示方法

--- Moment.js/main.js
+++ Moment.js/main.js
@@ -4,13 +4,19 @@ const d = moment('2018-01-10 15:35:24');
 // console.log(d.format('HH:mm:ss'));
 // console.log(d.format('hh:mm:ss a'));
 //hを小文字で１２時間表記。最後にaを付けると午前午後の表記
-d.locale('en');
-console.log(d.format('l'))
-console.log(d.format('ll'))
-console.log(d.format('lll'))
-console.log(d.format('llll'))
-console.log(d.format('L'))
-console.log(d.format('LL'))
-console.log(d.format('LLL'))
-console.log(d.format('LLLL'))
+// d.locale('en');
+// console.log(d.format('l'))
+// console.log(d.format('ll'))
+// console.log(d.format('lll'))
+// console.log(d.format('llll'))
+// console.log(d.format('L'))
+// console.log(d.format('LL'))
+// console.log(d.format('LLL'))
+// console.log(d.format('LLLL'))
+// console.log(moment('2018-1').daysInMonth());
+// console.log(moment('2018-2').daysInMonth());
+// console.log(d.toArray());//配列
+// console.log(d.toObject());//オブジェクト
+console.log(d.format('[Today]:L'))
+
 })();
