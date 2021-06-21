582e3ace356f0928b96c30c1a0164e0f3570d376
Turu0512 <hinataptyan@gmail.com>
2021-06-22 06:47:40 +0900

    add(),substract()

--- Moment.js/main.js
+++ Moment.js/main.js
@@ -2,12 +2,10 @@
 'use strict'
 const d = moment('2018-01-10 15:35:24');
 
-// console.log(d.set('year',2020).format('LLLL'));
-// //オブジェクト自体を書き換えてしまう
-// console.log(d.get('year'));
+// console.log(d.clone().add(3,'day').format('LLLL'));
+// console.log(d.clone().subtract(3,'year').format('LLLL'));
+//３年前
 
-console.log(d.clone().set('year',2020).format('LLLL'));
-//オブジェクト自体を書き換えてしまう
-console.log(d.get('year'));
-
+// console.log(d.clone().add(1,'y').add(2,'M').format('LLLL'));
+console.log(d.clone().add({y:1, M:2}).format('LLLL'));
 })();
