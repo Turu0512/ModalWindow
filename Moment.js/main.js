b632ba461f401477e192b6b797a4301fa98e7084
Turu0512 <hinataptyan@gmail.com>
2021-06-22 06:35:18 +0900

    日時情報の設定

--- Moment.js/main.js
+++ Moment.js/main.js
@@ -1,16 +1,13 @@
 (()=>{
 'use strict'
 const d = moment('2018-01-10 15:35:24');
+// d.set('year,2020');
+// d.month(11);
+// d.hour(1).minute(1);
+// d.day('木曜日');
+// d.month('3月');
 
-// console.log(d.get('y'))
-// console.log(d.get('M'))
-// console.log(d.get('D'))
-// console.log(d.get('d'))
-// console.log(d.get('h'))
-// console.log(d.get('m'))
-// console.log(d.get('s'))
-// console.log(d.get('ms'))
-console.log(d.year());
-console.log(d.month());
-console.log(d.date());
+
+console.log(d.format('LLLL'));
+
 })();
