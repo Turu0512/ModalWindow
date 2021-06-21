0a38d0a5841cdb55cd9471678fc8b1cd77eefcd5
Turu0512 <hinataptyan@gmail.com>
2021-06-21 20:57:36 +0900

    ｊｓのみでの表現

---
+++ Moment.js/main.js
@@ -0,0 +1,12 @@
+(()=>{
+'use strict'
+
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const p = document.querySelector('p')
+
+button.addEventListener('click',()=>{
+const d = new Date(input.value);
+  p.textContent = `${d.getFullYear()}年${d.getMonth()+1}月${d.getDate()}日`;
+});
+})();
