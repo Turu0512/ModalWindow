bdae835e3e76853121d13fd8c5893d8f03a3f9b7
Turu0512 <hinataptyan@gmail.com>
2021-06-22 06:41:50 +0900

    clone二よる日時の取得

--- Moment.js/main.js
+++ Moment.js/main.js
@@ -1,13 +1,13 @@
 (()=>{
 'use strict'
 const d = moment('2018-01-10 15:35:24');
-// d.set('year,2020');
-// d.month(11);
-// d.hour(1).minute(1);
-// d.day('木曜日');
-// d.month('3月');
 
+// console.log(d.set('year',2020).format('LLLL'));
+// //オブジェクト自体を書き換えてしまう
+// console.log(d.get('year'));
 
-console.log(d.format('LLLL'));
+console.log(d.clone().set('year',2020).format('LLLL'));
+//オブジェクト自体を書き換えてしまう
+console.log(d.get('year'));
 
 })();
