b86d1e52d6bdc1265596ba6e57f6ab37a8799d8e
Turu0512 <hinataptyan@gmail.com>
2021-06-23 05:58:29 +0900

    ボードの初期化

--- NumbersGame/main.js
+++ NumbersGame/main.js
@@ -3,16 +3,31 @@
 {
   class Panel {
     constructor() {
-      this.el = document.createElement('li')
+      this.el = document.createElement('li');
       this.el.classList.add('pressed');
     }
+    getEl() {
+      return this.el;
+    }
   }
-  class board {
+  class Board {
     constructor() {
       this.panels = [];
       for (let i = 0; i < 4; i++) {
         this.panels.push(new Panel())
       }
+      this.setup();
+    }
+
+    setup() {
+      const board = document.getElementById('board');
+      this.panels.forEach(panel => {
+        // board.appendChild(panel.el);
+        // クラスのプロパティーに直接アクセスしないほうがいい。メソット経由で取得
+        board.appendChild(panel.getEl());
+        // オブジェクト思考のカプセル化
+
+      });
     }
   }
 
