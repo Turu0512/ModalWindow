75cf890e2ab832230d7e592527c66e75875b738b
Turu0512 <hinataptyan@gmail.com>
2021-06-23 06:24:50 +0900

    ボタンを順番に押す実装

--- NumbersGame/main.js
+++ NumbersGame/main.js
@@ -5,6 +5,9 @@
     constructor() {
       this.el = document.createElement('li');
       this.el.classList.add('pressed');
+      this.el.addEventListener('click', () => {
+        this.check();
+      })
     }
     getEl() {
       return this.el;
@@ -12,6 +15,13 @@
     activate(num) {
       this.el.classList.remove('pressed');
       this.el.textContent = num;
+    }
+
+    check() {
+      if (currentNum === parseInt(this.el.textContent, 10)) {
+        this.el.classList.add('pressed');
+        currentNum++;
+      }
     }
   }
   class Board {
@@ -34,14 +44,24 @@
       });
     }
     activate() {
+      const nums = [0, 1, 2, 3];
       this.panels.forEach(panel => {
-        panel.activate(0);
+        const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
+        panel.activate(num);
+        //this.panelsに入ってるのは
+        // this.panels = [];
+        // for (let i = 0; i < 4; i++) {
+        //   this.panels.push(new Panel())
+        // }
+        // this.setup();
       });
     }
   }
 
   const board = new Board();
   // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+  let currentNum = 0;
+
   const btn = document.getElementById('btn');
   btn.addEventListener('click', () => {
     board.activate();
