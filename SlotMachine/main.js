eb74e810765b9a9a29bfdfb3c7350a9962a211ee
Turu0512 <hinataptyan@gmail.com>
2021-06-22 15:37:05 +0900

    stopの導入　色の変化　

--- SlotMachine/main.js
+++ SlotMachine/main.js
@@ -16,10 +16,20 @@ class Panel{
     this.stop.textContent = 'STOP';
     this.stop.classList.add('stop')
     this.stop.addEventListener('click',()=>{
+      if(this.stop.classList.contains('inactive')){
+        return;
+      }
+      this.stop.classList.add('inactive')
       clearTimeout(this.timeoutId);
       //clearTimeoutにtimeoutIdを渡すことでタイマーが止まる
-        })
+      panelsLeft--;
+
+      if(panelsLeft===0){//初期値が３でstopをすべて押し終えると発動
+        checkResult();
+      }
+        });
     //thisを使うのは他に呼び出されたときに対応するため
+
   section.appendChild(this.img);
   section.appendChild(this.stop);
 
@@ -43,7 +53,29 @@ return images[Math.floor(Math.random()*images.length)]
       this.spin();
     },50);
   }
+  isUnmatched(p1,p2){
+    //下のcheckResultのなかのisUnmatchedから引数を得ている。
+    return this.img.src !==p1.img.src && this.img.src !== p2.img.src;
+  }
+
+  unmatch(){
+    this.img.classList.add('unmatched');
+  }
 }
+function checkResult(){
+  if(panels[0].isUnmatched(panels[1],panels[2])){
+    panels[0].unmatch();
+  }
+  if(panels[1].isUnmatched(panels[0],panels[2])){
+    panels[1].unmatch();
+  }
+  if(panels[2].isUnmatched(panels[0],panels[1])){
+    panels[2].unmatch();
+  }
+}
+//(1) 左のパネル（panels[0]）が真ん中のパネル（panels[1]）と右のパネル（panels[2]）と同じか判定して、もし違っていたらpanels[0]にunmatchのクラスをつけて薄くする
+// (2) 真ん中のパネル（panels[1]）が左のパネル（panels[0]）と右のパネル（panels[2]）と同じか判定して、もし違っていたらpanels[1]にunmatchのクラスをつけて薄くする
+// (3) 右のパネル（panels[2]）が左のパネル（panels[0]）と真ん中のパネル（panels[1]）と同じか判定して、もし違っていたらpanels[1]にunmatchのクラスをつけて薄くする
 
 const panels=[
   new Panel(),
@@ -51,11 +83,18 @@ const panels=[
   new Panel(),
 ];
 
+let panelsLeft =3;
+
 const spin =document.getElementById('spin');
-spin.addEventListener('click',()=>{
-panels.forEach(panel =>{
-  panel.spin();
-  //panelにはclass Panelから値を引き出してるので、panel()とするだけで実行される。
+  spin.addEventListener('click',()=>{
+    if (spin.classList.contains('inactive')){
+      return;
+    }
+    spin.classList.add('inactive');
+  panels.forEach(panel =>{
+    panel.spin();
+    //panelにはclass Panelから値を引き出してるので、panel()とするだけで実行される。
+
 })
 });
 }
