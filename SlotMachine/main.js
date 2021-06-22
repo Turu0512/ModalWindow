8c3d84f5cd6ab6bc50985c63b661298b3ce85c00
Turu0512 <hinataptyan@gmail.com>
2021-06-22 12:04:55 +0900

    要素の挿入、画像の表示

--- SlotMachine/main.js
+++ SlotMachine/main.js
@@ -14,7 +14,17 @@ class Panel{
     this.stop.textContent = 'STOP';
     this.stop.classList.add('stop')
     //thisを使うのは他に呼び出されたときに対応するため
+  section.appendChild(this.img);
+  section.appendChild(this.stop);
+
+  const main = document.querySelector('main');
+  main.appendChild(section);
   }
 }
 
+const panels=[
+  new Panel(),
+  new Panel(),
+  new Panel(),
+];
 }
